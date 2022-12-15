181177d334989c16bc5eb7d48ea1b45d1ba62a57
andrenogueiragbi <andrepereiragbi@gmail.com>
2022-12-15 15:28:27 -0300

    pagina de login

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,5 +1,30 @@
+import styles from '../../styles/Admin.module.css'
+import Image from 'next/image'
+
 export default function About() {
-    return(
-        <h1>Em produção</h1>
+    return (
+        <div className={styles.container}>
+
+            <div className={styles.area_form} >
+
+                <div  className={styles.area_text_login} >
+                    <Image
+                        src='/images/login.png'
+                        width={40}
+                        height={40}
+                        alt='code'
+
+                    />
+                    <h1 className={styles.text_login}>Login</h1>
+
+                </div>
+
+                <input placeholder='email...' />
+                <input placeholder='senha...' />
+
+                <button>Entrar</button>
+            </div>
+
+        </div>
     )
 }
